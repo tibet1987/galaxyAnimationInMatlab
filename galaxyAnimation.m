2500567de46c12c89f7c28311772de1853c40710
Timo <thundy@web.de>
2021-01-20 22:42:14 +0100

    Update galaxyAnimation.m
    add gaussian distribution of stars in radial component (more stars in the inner part of the galaxy)

--- galaxyAnimation.m
+++ galaxyAnimation.m
@@ -1,25 +1,44 @@
 clear all, close all;
 clc;
 
-%% generate initial conditions
-%% trial 1 - uniform distribution with hard circular boundaries
+%% parameters
 L = 1;% Galaxy size
-N = 5000; % number of stars
-randVec = zeros(1,L);
-k=1;
-while k<=N % use a while loop to ensure that we get L values even some random numbers might lay outside the unit circle
-    randVec(1,k) = (rand(1,1)-0.5)*2;
-    randVec(2,k) = (rand(1,1)-0.5)*2;
-    if norm(randVec(:,k)) <= 1 % check if the generated random number lays within the unit circle
-        k = k+1;
+N = 2000; % number of stars
+starDistr = 2;  % start distribution:  1=uniform, 2=gaussian
+
+centerDensity = 1; % only for starDistr=2
+
+%% generate initial conditions
+if starDistr == 1
+    randVec = zeros(1,L);
+    k=1;
+    while k<=N % use a while loop to ensure that we get L values even some random numbers might lay outside the unit circle
+        randVec(1,k) = (rand(1,1)-0.5)*2;
+        randVec(2,k) = (rand(1,1)-0.5)*2;
+        if norm(randVec(:,k)) <= 1 % check if the generated random number lays within the unit circle
+            k = k+1;
+        end
     end
+
+    % mapping random numbers onto square
+    x = L*randVec(1,:);
+    y = L*randVec(2,:);
+    r = sqrt(x.^2 + y.^2);
+    phi = atan2(y,x);
+    
+elseif starDistr == 2
+    randVec(1,:) = randn(1,N)/3;
+    randVec(2,:) = rand(1,N)*2*pi;
+
+    r = randVec(1,:)/centerDensity*L;
+    phi = randVec(2,:);
+    subplot(211); hist(r,100);
+    subplot(212); hist(phi,100)
+    
+else
+    error('Wrong type of star distribution')
 end
 
-% mapping random numbers onto square
-x = L*randVec(1,:);
-y = L*randVec(2,:);
-r = sqrt(x.^2 + y.^2);
-phi = atan2(y,x);
 
 
 %% plotting
@@ -41,27 +60,31 @@ plot(d,calcOmega(d))
 
 
 %% every particle gets a velocity according to the velocity distribution function
+omega = zeros(1,N);
 for k=1:N
-    omega(k) = calcOmega(r(k));
+    omega(k) = calcOmega(abs(r(k)));
 end
 
 
 %% simulating a few time steps
 N_steps = 200;
 Ts = 0.1;
+L_plot = max(r);
 
 close all
 figure('color','k','position',[958    42   962   954])
 plotHandle = plot(r.*cos(phi),r.*sin(phi),'.w','Markersize',1);
 % plot(x,y,'.w','Markersize',10)
 set(gca,'color','k')
-xlim([-L,L])
-ylim([-L,L])
+xlim([-L_plot,L_plot])
+ylim([-L_plot,L_plot])
 axis equal
 hold all
 for k=1:N_steps
 	phi = phi + Ts*omega; % euler forward
     set(plotHandle,'XData',r.*cos(phi),'YData',r.*sin(phi))
+    xlim([-L_plot,L_plot])
+    ylim([-L_plot,L_plot])
     drawnow
     pause(0.05)
 end
